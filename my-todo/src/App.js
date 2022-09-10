7a1fb99c13fd4fceccf0fe0aff81cb98de9f6397
Sameer Mehmood <sameermehmood409@gmail.com>
2022-09-10 20:23:10 +0500

    Add existing project files to Git

--- my-todo/src/App.js
+++ my-todo/src/App.js
@@ -1,27 +1,37 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from './componenets/Form';
 import Header from './componenets/Header';
 import Todoslist from './componenets/Todoslist';
+import Login from './Components/Login/Login';
+import Home from './Components/Home/Home';
+import Signup from './Components/Signup/Signup';
+import { auth } from "./firebase";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RodoList from './Components/Todo';
+
+
+
+
 
 
 const App = () => {
-  const [input , setInput] = useState("");
-  const [inputEdit , setInputEdit] = useState("");
+  const [input, setInput] = useState("");
+  const [inputEdit, setInputEdit] = useState("");
 
-  const [todos , setTodos] = useState([]);
-  const [editTodo , setEditTodo] = useState(null);
+  const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
 
   const onUpdate = (e, text) => {
     const update = todos.map((t) => {
-      if(e.id === t.id){
-        return{
+      if (e.id === t.id) {
+        return {
           ...t,
           title: text,
           edit: false
         }
-      }else{
-        return{
+      } else {
+        return {
           ...t,
           edit: false
         }
@@ -29,34 +39,33 @@ const App = () => {
     })
     setTodos(update)
   }
-  console.log(todos,input, 'todos')
-  return (
-    <div className='container'>
-      <div className='app-wrapper'>
-        <div>
-          <Header />
-        </div>
-        <div>
-          <Form 
-          input={input}
-          setInput={setInput}
-          todos={todos}
-          setTodos={setTodos} 
-          editTodo={editTodo}
-          setEditTodo={setEditTodo}
-          />
-        </div>
-        <div>
-          <Todoslist  todos={todos} 
-          onUpdate={onUpdate}
-          setInputEdit={setInputEdit}
-          inputEdit={inputEdit}
-          setTodos={setTodos} setEditTodo={setEditTodo} />
-        </div>
-      </div>
+  console.log(todos, input, 'todos')
+  
+  const [userName, setUserName] = useState("");
+  
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserName(user.displayName);
+      } else setUserName("");
+    });
+  }, []);
 
-    </div>
+  return (
+    <Router>
+        <div className="App">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/todo" element={<RodoList />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/" element={<Home name={userName} />} />
+            </Routes>
+        </div>
+          </Router>
   );
 }
 
 export default App;
+
+
+
